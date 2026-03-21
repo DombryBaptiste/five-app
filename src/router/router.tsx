ac2e67262b1ba45cd0f16c5383f9deba86a97827
Baptiste <baptiste.dombry12@gmail.com>
2026-03-21 21:16:10 +0100

    hash router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,12 @@
 // src/router/router.tsx
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import ProtectedRoute from "../components/protectedRoute";
 import UserProfilPage from "../pages/UserProfilPage/UserProfilPage";
 import CalendarPage from "../pages/CalendarPage/CalendarPage";
 import CalendarGlobalPage from "../pages/CalendarGlobalPage/CalendarGlobalPage";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/login",
     element: <LoginPage />,
